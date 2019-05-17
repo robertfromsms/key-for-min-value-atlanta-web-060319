b8a52a49713415dccbcd5f3b46f9bf145a7895ae
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-17 18:42:32 +0000

    Automatically backed up by Learn

--- thing.rb
+++ thing.rb
@@ -1,12 +1,15 @@
-def thing(array)
+def my_sort(array1)
   i = 0
-  while i < array.length
-    yield(array[i], i)
-    i = i + 1
-  end
+  while i < array1.length
+    j = 0
+    while j < array1.length
+      if array2[i] > array2[j]
+        array1[i], array1[j] = array1[j], array1[i]
+      end
+      j += 1
+    end
+    i += 1
+  end  
 end
 
-thing([6,7,8,9]) {|a,b|
-  puts a
-  puts b
-}
+my_sort([9,8,7,6,5,4,3])
