4804b08d85649751ed1058b748bddb2585b2442f
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-17 20:03:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,35 @@
 def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
+  end
+  value_array = []
+  name_array = [] 
+  name_hash.each {|name, value|
+    value_array.push(value)
+    name_array.push(name)
+  }
 
+  def my_sort_mirroring(array1, array2)
+    if array1.length != array2.length
+      puts "arrays have to be the same size"
+      return "badbab"
+    end
+    loop {
+      swapped = false
+      (array2.length).times { |index|
+        if array2[index] > array2[index+1]
+          array2[i], array2[i+1] = array2[i+1], array2[i]
+          array1[i], array1[i+1] = array1[i+1], array1[i]
+          swapped = true
+        end
+      }
+      break if not swapped
+    }
+    return array1
+  end
+  if my_sort_mirroring(name_array, value_array) == "badbab"
+    puts "something wrong happened"
+    return
+  else
+    my_sort_mirroring(name_array, value_array)[0]
 end
