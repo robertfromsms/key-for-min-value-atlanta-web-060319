c6bf73f4ccbb9ec3fbe3189108dbdfacb49afc69
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-17 18:55:52 +0000

    Automatically backed up by Learn

--- thing.rb
+++ thing.rb
@@ -3,7 +3,10 @@ def my_sort(array1)
   while i < array1.length
     j = 0
     while j < array1.length
-      puts array1[i] > array1[j]
+      if array1[i] > array1[j]
+        array1[i], array1[j] = array1[j], array1[i]
+      end
+      puts array1
       j += 1
     end
     i += 1
